07084dba6ffe40c564b9cf3a2aa3d5704f073204
Abdul Muhaimin Bin Che Sohor <muhaimincs@gmail.com>
2019-10-14 11:42:58 +0800

    configure now

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,5 +1,6 @@
 const withTM = require("next-transpile-modules");
 
 module.exports = withTM({
-  transpileModules: ["launcher", "chatbox"]
+  transpileModules: ["launcher", "chatbox"],
+  distDir: "build"
 });
